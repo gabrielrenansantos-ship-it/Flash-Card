605079fefa3f1628c541f8f73498817b2becd914
gabrielrenansantos-ship-it <gabriel.renan.santos@escola.pr.gov.br>
2026-07-03 08:43:48 -0300

    67

--- perguntas.js
+++ perguntas.js
@@ -2,6 +2,40 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
+    'História 1',
+    'Fale 2 tipos de fontes históricas ',
+    'fontes escritas e vestigios arqueológicos '
+)
+
+criaCartao(
+    'História 2',
+    'Oque são fontes materiais? ',
+    'Onde a fonte é o próprio suporte '
+)
+
+criaCartao(
+    'Sociologia 1',
+    'Oque é modernidade?',
+    'Um modo de vida que chamamos de sistema econômico.'
+)
+criaCartao(
+    'Sociologia 2',
+    'Quais os pilares da ciências sociais?',
+    'Sociologia,Centropologia e ciencia da politica'
+)
+
+criaCartao(
+    'Filosofia 1',
+    'A filosofia surgiu da vida?',
+    'não'
+)
+
+criaCartao(
+    'Filosofia 2',
+    'De onde a filosofia nasceu?',
+    'Ela foi inventada para lidar com os problemas que a vida na Pólis criou.'
+)
+criaCartao(
     'Disciplina 1',
     'Pergunta 1',
     'Resposta 1'
@@ -34,58 +68,6 @@ criaCartao(
     'Disciplina 3',
     'Pergunta 3',
     'Resposta 3'
-)
-criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
-)
-
-criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
-)
-
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
-)
-
-criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
-)
-
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
-)
-
-criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
-)
-
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-
 )
 criaCartao(
     'Disciplina 1',
@@ -157,4 +139,16 @@ criaCartao(
     'Disciplina 3',
     'Pergunta 3',
     'Resposta 3'
+
+)
+criaCartao(
+    'Disciplina 1',
+    'Pergunta 1',
+    'Resposta 1'
+)
+
+criaCartao(
+    'Disciplina 2',
+    'Pergunta 2',
+    'Resposta 2'
 )
